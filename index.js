1b77de844bf97d94905ab2d63ee24498fa8404da
Erik Stone <spologas@gmail.com>
2021-07-23 23:25:24 -0700

    updated both index files and tried some troubleshooting

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const fs = require ('fs');
+const path = require ('path');
+const { employeeEntry, managerData, engineerData, internData } = require('./helperUtilities/employeeEntry');
+const { createHTML } = require('./helperUtilities/createHTML');
+
+function writeFile (data) {
+    fs.writeFile("index.html", data,
+    (err) => err ? console.error(err) : console.log('index.html is completed'))
+}
+
+async function doIt () {
+    await employeeEntry();
+    writeFile(createHTML(managerData, engineerData, internData));
+}
+
+doIt();
