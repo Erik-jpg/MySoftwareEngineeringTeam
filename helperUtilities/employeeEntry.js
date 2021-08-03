34a66520245588b5d5308d025f31cb7e5a6c34a0
Erik Stone <spologas@gmail.com>
2021-08-02 17:49:43 -0700

    added some more functions

--- helperUtilities/employeeEntry.js
+++ helperUtilities/employeeEntry.js
@@ -1,21 +1,109 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const path = require('path');
 
+const managerData = [];
+const engineerData = [];
+const internData = [];
 class Employee {
     constructor(name, id, email, role) {
         this.name= name;
         this.id= id;
         this.email= email;
         this.role= role;
-}
+    }
 getName() { return `${this.name}`}
 getId() { return `${this.id}`}
 getEmail() { return `${this.email}`}
 getRole() { return `${this.role}`}
 }
 
-class Manager extends Employee {
-    constructor(name, id, email, role, officeNumber) {
-        
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is this employee's name?",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is this employee's id number?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is this employee's email address?",
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "What is this employee's role?",
+        choices: ["Manager", "Engineer", "Intern"],
     }
-}
+];
+
+const generateEmployees = () => {
+    inquirer.prompt(questions).then((role) => {
+        inquirer.prompt([
+            {
+                when: () => role === "Manager",
+                type: "input",
+                name: "officeNumber",
+                message: "What is this employee's office number?",
+            },
+            {
+                when: () => role === "Engineer",
+                type: "input",
+                name: "github",
+                message: "What is this employee's Github user name?",
+            },
+            {
+                when: () => role === "Intern",
+                type: "input",
+                name: "school",
+                message: "Where does this Intern attend school?",
+            },
+            {
+                type: "confirm",
+                name: "addToTeam",
+                message: "Would you like to add someone to the team roster?",
+            }, 
+        ])
+    })
+};
+        inquirer.then((answers) => {
+            if (answers.role === "Manager") {
+                const manager = new Manger(`${this.name} ${this.id} ${this.email} ${this.role} ${this.officeNumber}`);
+                managerData.push(manager);
+            }
+            if (answers.role === "Engineer") {
+                const engineer = new Engineer(`${this.name} ${this.id} ${this.email} ${this.role} ${this.github}`);
+                engineerData.push(Engineer);
+            }
+            if (answers.role === "Intern") {
+                const intern = new Intern(`${this.name} ${this.id} ${this.email} ${this.role} ${this.school}`);
+                internData.push(Intern);
+            }
+            {
+                if (answers === "addToTeam") {
+                    generateEmployees();
+                }
+                else {
+                    team.forEach((team) => {
+                        console.log(team);
+                    });
+                }
+                function writingEmployees (fileName, data) {
+                fs.writeFile(path.join(process.cwd(), fileName, data, (err) => {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log('file written');
+                }))
+            }
+        }),
+
+
+
+        generateEmployees();
+        console.log('working so far');
