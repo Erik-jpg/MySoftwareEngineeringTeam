1d3f956315579ac2d75ef1c49e38d1dac592a018
Erik Stone <spologas@gmail.com>
2021-08-10 22:28:03 -0700

    added switch statemnet and fixed sql tables

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
-const managerData = require('../helperUtilities/managerData');
-const engineerData = require('../helperUtilities/engineerData');
-const inturnData = require('../helperUtilities/inturnData');
+const managerData = require('../helperUtilities/employeeEntry/managerData');
+const engineerData = require('../helperUtilities/employeeEntry/engineerData');
+const inturnData = require('../helperUtilities/employeeEntry/inturnData');
 const generateEmployees = require('../helperUtilities/generateEmployees');
-const createHTML = require('../helperUtilities/createHTML');
+const createHTML = require('../helperUtilities/employeeEntry/createHTML');
 
 
 
